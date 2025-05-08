fc672180fbd2ae66faf8a9e6d18f62ee0bf36a15
Adam Harmaniak <125898179+adamharmaniak@users.noreply.github.com>
2025-05-09 00:49:12 +0200

    Add files via upload

---
+++ BiDistrib/source/ui.R
@@ -0,0 +1,39 @@
+source("global.R")
+
+ui <- fluidPage(
+  theme = shinytheme("slate"),
+  tags$head(
+    tags$link(rel = "stylesheet", type = "text/css", href = "styles.css")
+  ),
+  
+  # Horne menu (prepina len sidebar, vystup ostava rovnaky)
+  tabsetPanel(id = "main_tabs",
+              tabPanel("Data selection", value = "data"),
+              tabPanel("Density model", value = "joint_density"),
+              tabPanel("Regression", value = "regression"),
+              tabPanel("Classification", value = "classification"),
+              tabPanel("Conditional Densities", value = "conditional_densities")
+  ),
+  
+  # Spolocne rozlozenie
+  sidebarLayout(
+    sidebarPanel(
+      uiOutput("tools_sidebar")
+    ),
+    mainPanel(
+      div(
+        id = "model_outputs",
+        style = "padding: 10px; margin-bottom: 30px;",
+        uiOutput("model_outputs_combined")
+      ),
+      
+      div(
+        id = "model_summary",
+        style = "padding: 10px; margin-bottom: 30px;",
+        uiOutput("model_summary_ui")
+      ),
+      
+      uiOutput("variable_info_ui")
+    )
+  )
+)
